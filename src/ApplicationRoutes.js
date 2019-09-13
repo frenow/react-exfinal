b108bd22f772696460eb9131cbc14d84fe45b72e
frenow <frenow@gmail.com>
2019-09-13 15:39:05 -0300

    fix model car3

--- src/ApplicationRoutes.js
+++ src/ApplicationRoutes.js
@@ -1,9 +1,11 @@
 import React from "react";
 import { Route } from "react-router-dom";
 import Home from "./components/home";
+import Passo1 from "./components/passo1";
 
 export const ApplicationRoutes = () => (
     <>
     <Route path="/" exact component={Home} />
+	<Route path="/:id" exact component={Passo1} />
     </>
 );
